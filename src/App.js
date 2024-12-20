6275bf8aa3938161a23ed85c1721b523ce0019d9
jr132662 <cooljon08@gmail.com>
2024-12-20 18:42:01 -0500

    attack on titan api initialized

--- src/App.js
+++ src/App.js
@@ -4,31 +4,39 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
-      monsters: [
-        {
-          name: 'Jona',
-        },
-        {
-          name: 'Grey',
-        },
-        {
-          name: 'Eren',
-        }
-      ]
+      characters: [],
     };
   }
-  render () {
+
+  componentDidMount() {
+    fetch('https://api.attackontitanapi.com/characters')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data); // Inspect the API response
+        this.setState(() => {
+          return { characters: data.results }; // Extract the 'results' array
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
+
+  render() {
     return (
       <div className="App">
-        <h1>Monster Rolodex</h1>
-        {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>
-        })}
+        {this.state.characters.map((character) => {
+        return (
+          <div key={character.id}>
+            <h1>{character.name}</h1>
+          </div>
+        );
+      })}
       </div>
     );
   }
 }
-
 
 export default App;
